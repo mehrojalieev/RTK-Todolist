26c4da817a77b30110aac15c74459cb6fdf4690d
mehrojalieev <aliyevmehrojbek0@gmail.com>
2023-11-27 16:40:23 +0500

    Connected uuid and data

--- src/components/todo-container/TodoContainer.jsx
+++ src/components/todo-container/TodoContainer.jsx
@@ -1,12 +1,27 @@
 import "./TodoContainer.scss"
+import { useCreateTaskMutation } from "../../api/taskSlice"
+import { useState } from "react"
+import { v4 as uuidv4 } from 'uuid';
+
 
 const TodoContainer = () => {
+  const [createTask] = useCreateTaskMutation()
+
+  const [taskName, setTaskName] = useState('')
+
+  const handleCreateTask = (e) => {
+    e.preventDefault()
+      const newTask = {
+        id: uuidv4,
+        name: taskName
+      }
+  }
 
   return (
     <div className='todo-container'>
         <div className="create__task-form">
-          <form>
-            <input type="text" placeholder="Enter Task" />
+          <form onSubmit={handleCreateTask}>
+            <input value={taskName} onChange={(e) => setTaskName(e.target.value)} type="text" placeholder="Enter Task" />
             <button>Add</button>
           </form>
         </div>
